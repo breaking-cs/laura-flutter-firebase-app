32defc023fd35c864fdcf0b1b4ecc5f661ab40c2
hayoung0Lee <hayoung0.lee@gmail.com>
2021-09-19 22:18:31 +0900

    Tab 구조화

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,64 @@
 import 'package:flutter/material.dart';
+import "./screens/home.dart";
+import "./screens/customers.dart";
+import "./screens/gallery.dart";
+import "./screens/settings.dart";
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+
+  final _tabs = const [
+    Center(child: Home()),
+    Center(child: Customers()),
+    Center(child: Gallery()),
+    Center(child: Settings()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Laura"),
-        centerTitle: true,
-      ),
-      bottomNavigationBar: BottomAppBar(
-          color: Colors.blue,
-          child: IconButton(
-              icon: Icon(Icons.home),
-              onPressed: () => print("hello"),
-              color: Colors.white)),
-    ));
+            appBar: AppBar(
+              title: const Text("Laura"),
+              centerTitle: true,
+            ),
+            body: _tabs[_currentIndex],
+            bottomNavigationBar: BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                currentIndex: _currentIndex,
+                items: const [
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: "Home",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.account_circle),
+                    label: "Customers",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.camera),
+                    label: "Gallery",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.settings),
+                    label: "Settings",
+                  ),
+                ],
+                onTap: (index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                })));
   }
 }
