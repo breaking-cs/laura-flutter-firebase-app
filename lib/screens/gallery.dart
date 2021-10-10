ee2b2cbb924adb540443543393926a8572d07e96
hayoung0Lee <hayoung0.lee@gmail.com>
2021-10-10 21:27:55 +0900

    Gallery GridView 프로토타입

--- lib/screens/gallery.dart
+++ lib/screens/gallery.dart
@@ -1,5 +1,6 @@
 import 'package:app/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import '../models/transaction.dart';
 
 class Gallery extends StatelessWidget {
@@ -12,6 +13,7 @@ class Gallery extends StatelessWidget {
       customerId: index + 1,
       amount: 1000,
       date: DateTime.now(),
+      imgUrl: 'https://picsum.photos/400',
     ),
   );
 
@@ -21,7 +23,11 @@ class Gallery extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: transactions.length,
       itemBuilder: (ctx, index) {
         var tx = transactions[index];
 
@@ -31,57 +37,32 @@ class Gallery extends StatelessWidget {
             horizontal: 10,
           ),
           elevation: 4,
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.fromLTRB(
-                  10,
-                  30,
-                  30,
-                  30,
-                ),
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.blueGrey,
-                    width: 2,
-                  ),
-                ),
-                child: Text(
-                  tx.amount.toString(),
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                    color: Colors.blueGrey,
-                  ),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(tx.imgUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+            alignment: Alignment.bottomRight,
+            child: Container(
+              width: double.infinity,
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.only(right: 10),
+              color: Colors.grey,
+              height: 30,
+              child: Text(
+                tx.customerId.toString() + "번 고객님의 꽃",
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Colors.white,
                 ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tx.customerId.toString() + "번 고객님의 꽃",
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                      color: Colors.blueGrey,
-                    ),
-                  ),
-                  Text(
-                    formatDate(tx.date),
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.blueGrey,
-                    ),
-                  ),
-                ],
-              ),
-            ],
+            ),
           ),
         );
       },
-      itemCount: transactions.length,
     );
   }
 }
