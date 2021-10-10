ef2379d4e30449286503ac413fb895ca8d18de36
hayoung0Lee <hayoung0.lee@gmail.com>
2021-10-10 16:40:42 +0900

    home 화면 카드 여러개에 더보기 버튼까지 프로토타입

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,62 @@
 import 'package:flutter/material.dart';
 
+class CustomCard extends StatelessWidget {
+  const CustomCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      margin: const EdgeInsets.only(
+        bottom: 30,
+        left: 10,
+        right: 10,
+      ),
+      child: Text("Hello"),
+      elevation: 5,
+    );
+  }
+}
+
+class HomeCard extends StatelessWidget {
+  const HomeCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const Text("Today: Date"),
+          GridView.count(
+            padding: const EdgeInsets.only(
+              top: 20,
+            ),
+            primary: true,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            crossAxisCount: 2,
+            children: const <Widget>[
+              CustomCard(),
+              CustomCard(),
+              CustomCard(),
+              CustomCard(),
+            ],
+          ),
+          const ElevatedButton(
+            onPressed: null,
+            child: Text("더 보기"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -25,11 +82,7 @@ class Home extends StatelessWidget {
               left: 10,
               right: 10,
             ),
-            child: Container(
-              padding: const EdgeInsets.all(20.0),
-              width: double.infinity,
-              child: Text("Chart"),
-            ),
+            child: const HomeCard(),
             elevation: 5,
           ),
         ),
