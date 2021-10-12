e162ac7af923839b4cf06e60ab9b520d34b91790
jjk-dev <giyen10@gmail.com>
2021-10-12 21:01:33 +0900

    move(settings): logout 이동 및 auth, dialog 추가

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,41 +1,92 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:material_dialogs/widgets/buttons/icon_button.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:material_dialogs/material_dialogs.dart';
+import '../utils/auth_service.dart';
 import './login.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
+
+  /*
+  final bool isLoggedIn;
+  const Settings(this.isLoggedIn);
+   */
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  //late bool isLoggedIn;
+
+  Future<void> requestLogOut() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      prefs.remove("user_id");
+
+      Authentication.signOut().then((res) {
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => Login()),
+            // MaterialPageRoute(builder: (context) => Login(isLoggedIn)),
+                (Route<dynamic> route) => false);
+      });
+      /*
+      setState(() {
+        isLoggedIn = false;
+      });
+       */
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Settings",
-          style: TextStyle(color: Colors.black),
-        ),
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-      ),
-      extendBodyBehindAppBar: true,
       body: ListView(
         children: <Widget>[
           ListTile(
-            title: Text("Logout",
-              style: TextStyle(
-                  fontSize: 30),
-              ),
+            title: const Text('Edit Profile'),
             onTap: () {
-              FirebaseAuth _auth = FirebaseAuth.instance;
-              _auth.signOut().then((res) {
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context) => Login()),
-                        (Route<dynamic> route) => false);
-              });
+              // TODO: editProfile();
             },
           ),
-          ],
-        ),
+          ListTile(
+            title: const Text('Logout'),
+            leading: const Icon(Icons.logout,),
+            onTap: () {
+              Dialogs.bottomMaterialDialog(
+                  msg: 'Are you sure to logout?',
+                  title: 'Logout',
+                  context: context,
+                  actions: [
+                    IconsButton(
+                      text: 'Cancel',
+                      iconData: Icons.cancel_outlined,
+                      color: Colors.grey,
+                      textStyle: const TextStyle(color: Colors.white),
+                      iconColor: Colors.white,
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    IconsButton(
+                      text: 'Logout',
+                      iconData: Icons.logout,
+                      color: Colors.redAccent,
+                      textStyle: const TextStyle(color: Colors.white),
+                      iconColor: Colors.white,
+                      onPressed: () {
+                        requestLogOut();
+                      },
+                    ),
+                  ],
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
