5574bc7c4ce87cd9583b09b1c7db31468927946c
hayoung0Lee <hayoung0.lee@gmail.com>
2021-11-13 04:19:38 +0900

    image 선택하고 업로드하고 view 하는것까지

--- lib/providers/transactions.dart
+++ lib/providers/transactions.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../models/customer.dart';
 import '../models/transaction.dart' as tx;
+import 'package:path/path.dart';
 
 class TransactionStream {
   late final FirebaseFirestore _fireStoreDatabase;
@@ -20,37 +23,56 @@ class TransactionStream {
     } else {
       return txs!
           .orderBy(
-        'createdAt',
-        descending: false,
-      )
+            'createdAt',
+            descending: false,
+          )
           .snapshots()
           .map((snapshot) => snapshot.docs
-          .map((doc) => tx.Transaction.fromJson(
-          doc.data() as Map<String, dynamic>, doc.id))
-          .toList());
+              .map((doc) => tx.Transaction.fromJson(
+                  doc.data() as Map<String, dynamic>, doc.id))
+              .toList());
     }
   }
 }
 
-Future<void> addTransactions(
-    String customerId, {
-      required int amount,
-      required String memo,
-      String imgUrl = "",
-    }) async {
+Future<String> uploadFile(String customerId, File imageFile) async {
+  firebase_storage.TaskSnapshot uploadTask;
+  String fileName = basename(imageFile.path);
+  // Create a Reference to the file
+  firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance
+      .ref()
+      .child('$customerId/$fileName');
 
+  uploadTask = await ref.putFile(imageFile);
+
+  String downloadURL = await uploadTask.ref.getDownloadURL();
+
+  return downloadURL;
+}
+
+Future<void> addTransactions(
+  String customerId, {
+  required int amount,
+  required String memo,
+  File? imageFile,
+}) async {
   late final CollectionReference? txs;
   FirebaseFirestore _fireStoreDatabase = FirebaseFirestore.instance;
   txs = _fireStoreDatabase.collection('tx/${customerId}/purchase');
 
+  // upload image
+  String? link;
+  if (imageFile != null) {
+    link = await uploadFile(customerId, imageFile);
+  }
+
   return txs
       .add({
-    'amount': amount,
-    "memo": memo,
-    "imgUrl": imgUrl,
-    'createdAt': Timestamp.now(),
-  })
-
+        'amount': amount,
+        "memo": memo,
+        "imgUrl": link ?? "",
+        'createdAt': Timestamp.now(),
+      })
       .then((val) => print('customer Added'))
       .catchError((error) => print("Failed to Add Customers: $error"));
 }
