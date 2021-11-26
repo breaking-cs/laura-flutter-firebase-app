25308f8edbbe5cbd69257c8087067767658e376b
jjk-dev <giyen10@gmail.com>
2021-11-26 13:05:06 +0900

    chore(login)

--- lib/screens/email_signup.dart
+++ lib/screens/email_signup.dart
@@ -43,6 +43,14 @@ class _EmailSignUpState extends State<EmailSignUp> {
               ),
             ],
           );
+      }).catchError((err) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              duration: const Duration(seconds: 2),
+              content: Text(err.message),
+              backgroundColor: Colors.red,
+            ),
+          );
       });
   }
 
@@ -52,7 +60,8 @@ class _EmailSignUpState extends State<EmailSignUp> {
       resizeToAvoidBottomInset : true,
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('회원가입',
+        title: const Text(
+          '회원가입',
           style: TextStyle(color: Colors.black),
         ),
         backgroundColor: Colors.transparent,
@@ -62,127 +71,127 @@ class _EmailSignUpState extends State<EmailSignUp> {
         ),
       ),
       body: SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 32.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                controller: _userName,
-                keyboardType: TextInputType.text,
-                maxLength: 10,
-                textCapitalization: TextCapitalization.characters,
-                decoration: const InputDecoration(
-                  labelText: 'Username',
-                  labelStyle: TextStyle(color: Colors.indigo),
-                  hintText: 'Enter your username',
-                  suffixIcon: Icon(
-                    Icons.account_circle,
-                    color: Colors.grey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  controller: _userName,
+                  keyboardType: TextInputType.text,
+                  maxLength: 10,
+                  textCapitalization: TextCapitalization.characters,
+                  decoration: const InputDecoration(
+                    labelText: 'Username',
+                    labelStyle: TextStyle(color: Colors.indigo),
+                    hintText: 'Enter your username',
+                    suffixIcon: Icon(
+                      Icons.account_circle,
+                      color: Colors.grey,
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide:BorderSide(
+                          color: Colors.indigo,
+                          width: 2.0),
+                    ),
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide:BorderSide(
-                        color: Colors.indigo,
-                        width: 2.0),
-                  ),
+                  validator: (input) {
+                    if (input!.isEmpty) {
+                      return 'Enter your username';
+                    }
+                    return null;
+                  },
                 ),
-                validator: (input) {
-                  if (input!.isEmpty) {
-                    return 'Enter your username';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _userEmail,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Colors.indigo),
-                  hintText: 'Enter your email address',
-                  suffixIcon: Icon(
-                    Icons.email,
-                    color: Colors.grey,
+                TextFormField(
+                  controller: _userEmail,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.indigo),
+                    hintText: 'Enter your email address',
+                    suffixIcon: Icon(
+                      Icons.email,
+                      color: Colors.grey,
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide:BorderSide(
+                          color: Colors.indigo,
+                          width: 2.0),
+                    ),
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide:BorderSide(
-                        color: Colors.indigo,
-                        width: 2.0),
+                  validator: (input) {
+                    if (input!.isEmpty) {
+                      return 'Enter your email address';
+                    } else if (!input.contains('@')) {
+                      return 'Please enter a valid email address!';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _userPassword,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.indigo),
+                    hintText: 'Enter your password',
+                    suffixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.grey,
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide:BorderSide(
+                          color: Colors.indigo,
+                          width: 2.0),
+                    ),
                   ),
+                  validator: (input) {
+                    if (input!.isEmpty) {
+                      return 'Enter your password';
+                    } else if (input.length < 6) {
+                      return 'Password must be at least 6 characters!';
+                    }
+                    return null;
+                  },
                 ),
-                validator: (input) {
-                  if (input!.isEmpty) {
-                    return 'Enter your email address';
-                  } else if (!input.contains('@')) {
-                    return 'Please enter a valid email address!';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                controller: _userPassword,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: TextStyle(color: Colors.indigo),
-                  hintText: 'Enter your password',
-                  suffixIcon: Icon(
-                    Icons.lock,
-                    color: Colors.grey,
+                const SizedBox(height: 60),
+                IconsButton(
+                  padding: const EdgeInsets.fromLTRB(80, 15, 80, 15),
+                  text: 'Register',
+                  color: Colors.indigo,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusDirectional.circular(10),
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide:BorderSide(
-                        color: Colors.indigo,
-                        width: 2.0),
+                  textStyle: const TextStyle(color: Colors.white,
+                      fontSize: 20),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      requestSignUp();
+                    }
+                  },
+                ),
+                const SizedBox(height: 30),
+                GestureDetector(
+                  child: const Text('Already have an account? CLICK HERE',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 15,
+                        decoration: TextDecoration.underline),
                   ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Login()),
+                    );
+                  },
                 ),
-                validator: (input) {
-                  if (input!.isEmpty) {
-                    return 'Enter your password';
-                  } else if (input.length < 6) {
-                    return 'Password must be at least 6 characters!';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 60),
-              IconsButton(
-                padding: const EdgeInsets.fromLTRB(80, 15, 80, 15),
-                text: 'Register',
-                color: Colors.indigo,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadiusDirectional.circular(10),
-                ),
-                textStyle: const TextStyle(color: Colors.white,
-                    fontSize: 20),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    requestSignUp();
-                  }
-                },
-              ),
-              const SizedBox(height: 30),
-              GestureDetector(
-                child: const Text('Already have an account? CLICK HERE',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 15,
-                      decoration: TextDecoration.underline),
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Login()),
-                  );
-                },
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
       ),
 
     );
